89e4ca2573d9ae294bd5dd255590c73871adcc1a
Salomão <101257752+5alomao@users.noreply.github.com>
2025-05-13 08:30:50 -0300

    refactor: Changed project name

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 document.getElementById("navbar").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-  <a class="navbar-brand text-orange" href="index.php">FitCrud</a>
+  <a class="navbar-brand text-orange" href="index.php">PhPeso</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navLinks">
     <span class="navbar-toggler-icon"></span>
   </button>

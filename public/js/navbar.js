8e530bf86b20d1a1d4450b041dc7b39f3111cf02
5alomao <salomao.junior2k22@gmail.com>
2025-06-08 13:25:51 -0300

    refactor: Ajustado url de navegação da navbar.

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,14 +1,14 @@
 document.getElementById("navbar").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-  <a class="navbar-brand text-orange" href="index.php">PhPeso</a>
+  <a class="navbar-brand text-orange" href="/fit-crud/index.php">PhPeso</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navLinks">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navLinks">
     <ul class="navbar-nav ms-auto">
-      <li class="nav-item"><a class="nav-link" href="workouts.php">Treinos</a></li>
-      <li class="nav-item"><a class="nav-link" href="exercises.php">Exercícios</a></li>
-      <li class="nav-item"><a class="nav-link" href="login.php">Logout</a></li>
+      <li class="nav-item"><a class="nav-link" href="/fit-crud/app/view/workout/workouts.php">Treinos</a></li>
+      <li class="nav-item"><a class="nav-link" href="/fit-crud/app/view/exercise/exercises.php">Exercícios</a></li>
+      <li class="nav-item"><a class="nav-link" href="/fit-crud/app/view/user/login.php">Logout</a></li>
     </ul>
   </div>
 </nav>

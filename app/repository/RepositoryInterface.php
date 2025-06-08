7bd9ce3c4dbf4e8833923fbac8e9d1b88a469ade
5alomao <salomao.junior2k22@gmail.com>
2025-06-07 23:14:46 -0300

    feat: Add id parameter in update function.

--- app/repository/RepositoryInterface.php
+++ app/repository/RepositoryInterface.php
@@ -3,9 +3,7 @@
 interface RepositoryInterface
 {
     public function insert(object $entity): bool;
-    public function update(object $entity): bool;
+    public function update(int $id, object $entity): bool;
     public function delete(int $id): bool;
     public function selectAll(): array;
 }
-
-?>
--- app/service/ServiceInterface.php
+++ app/service/ServiceInterface.php
@@ -3,8 +3,7 @@
 interface ServiceInterface
 {
     public function insert(array $data): bool;
-    public function update(array $data): bool;
+    public function update(int $id, array $data): bool;
     public function delete(int $id): bool;
     public function selectAll(): array;
 }
-?>

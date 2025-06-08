11b432796fddbde453d7c32da76dd241617f0ea4
5alomao <salomao.junior2k22@gmail.com>
2025-06-07 23:08:54 -0300

    refactor: Removing and moving files to apply MVC design pattern.

--- app/exception/user/ValidationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ValidationException extends Exception
-{
-    public function __construct(string $message = "Erro de validação: ", string $error, int $code = 422)
-    {
-        parent::__construct("$message . $error", $code);
-    }
-}
-?>

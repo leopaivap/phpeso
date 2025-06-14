fa8a9dda3adb61cd067d7c1dca12dd45bd778129
5alomao <salomao.junior2k22@gmail.com>
2025-06-14 11:19:28 -0300

    refactor: Adjusted styles import

--- app/view/user/register.php
+++ app/view/user/register.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="../../../public/css/style.css" />
-  <link rel="stylesheet" type="text/css" href="../../../public/css/register.css" />
 </head>
 
 <body>

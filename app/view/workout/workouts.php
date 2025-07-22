4cacad0e90c29bc5fa34f7e95061926a5e5c5206
Salomão <101257752+5alomao@users.noreply.github.com>
2025-07-22 08:14:47 -0300

    style: modal header adjusted

--- app/view/workout/workouts.php
+++ app/view/workout/workouts.php
@@ -124,8 +124,8 @@
   <div class="modal fade" id="workoutModal" tabindex="-1">
     <div class="modal-dialog modal-lg modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Detalhes do Treino</h5><button type="button" class="btn-close"
+        <div class="modal-header" style="background: #333333; color: #fefefe">
+          <h5 class="modal-title">Detalhes do Treino</h5><button type="button" class="btn-close bg-white"
             data-bs-dismiss="modal"></button>
         </div>
         <div class="modal-body">

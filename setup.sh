f4c5c26bb5ca47031768a148bfaea9b0ba96cc89
Leonardo Paiva <paivaleo7@gmail.com>
2025-07-21 23:41:57 -0300

    trying to connect railway

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "Aguardando MySQL estar disponível..."
-
-until mysqladmin ping -h mysql -u root --silent; do
+# USA AS VARIÁVEIS DA RAILWAY PARA ESPERAR PELO BANCO DE DADOS
+echo "Aguardando MySQL estar disponível na Railway..."
+until mysqladmin ping -h$MYSQLHOST -P$MYSQLPORT -u$MYSQLUSER -p$MYSQLPASSWORD --silent; do
+  echo "Ainda aguardando..."
   sleep 2
 done
 
-echo "Executando setup.php..."
-php /var/www/html/setup.php
+echo "Executando setup.php para criar tabelas..."
+php /var/www/html/app/repository/create-table.php
+php /var/www/html/app/repository/muscle-group/populate-muscle-groups.php
 
 echo "Iniciando Apache..."
 exec apache2-foreground

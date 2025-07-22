f7d11fe4051cb5f0cf4c4d90855f6d54b992c565
Salomão Junior <salomao.junior2k22@gmail.com>
2025-07-22 08:39:55 -0300

    fix: added footer workouts page

--- app/view/workout/workouts.php
+++ app/view/workout/workouts.php
@@ -142,6 +142,8 @@
     </div>
   </div>
 
+  <?php include_once __DIR__ . '/../templates/footer.php'; ?>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     function translateDifficulty(level) {
